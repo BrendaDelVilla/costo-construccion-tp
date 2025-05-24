d37fdf13fcabdb1d6036329905185beefd0615e4
BrendaDelVilla <brendaunlam18@gmail.com>
2025-05-24 05:28:08 -0300

    Create convertir_coma.c
    creación de la segunda función- convertir la coma a punto

---
+++ lib-costo-construccion/convertir_coma.c
@@ -0,0 +1,24 @@
+void convertir_Coma(char *IndiceCad)
+{
+    int convertido=0;
+    char *PosComa=NULL;
+    while(*IndiceCad!='\0')
+        {
+           if(*IndiceCad==',')
+              {
+                convertido++;
+                PosComa=IndiceCad;
+
+              }
+          IndiceCad++;
+        }
+    if(convertido==1)
+           {
+                *PosComa='.';
+           }
+    else
+          {
+             //hay mas de una coma - ( si es 0 no hay coma)
+          }
+
+}
